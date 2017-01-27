1da8bc6bea4aa3a2a09ce91c7d5e5c14ff7bc189
Leena Rex <lrex@silverlink.com>
2017-01-27 14:02:49 -0500

    Add variable playgroung

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+var person = {
+	name: 'Andrew',
+	age: 21
+};
+
+function updatePerson(obj) {
+	// obj = {
+	// 	name: 'Andrew',
+	// 	age: 24
+	// 
+	obj.age = 24;
+
+}
+
+updatePerson(person);
+console.log(person);
+
+// Array Example
+
+var grades = [90, 85];
+
+function addGrades(grades){
+	grades.push(15);
+	debugger;
+	
+}
+
+addGrades(grades);
+console.log(grades)
+
+
