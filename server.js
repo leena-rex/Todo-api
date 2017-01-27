4a416e4847ab3b246fceabb6efbea1bb99af4e2d
Leena Rex <lrex@silverlink.com>
2017-01-27 14:21:23 -0500

    param query

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function (req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -24,6 +24,12 @@ app.get('/todos', function (req, res) {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
 
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ){
+		filteredTodos = _.filter(filteredTodos, function( todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+ 
 	res.json(filteredTodos);
 });
 
