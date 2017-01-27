660d4a53ab9e9cab401df797a74b79c03e29ca7b
Leena Rex <lrex@silverlink.com>
2017-01-27 14:02:11 -0500

    Added filter

--- server.js
+++ server.js
@@ -18,8 +18,28 @@ app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
+// GET /todos?completed=true
+app.get('/todos', function (req, res){
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	
+	// if has property && complted === 'true'
+	// call filteredTodos = _.where(filteredTodos, ?)
+	// else if has prop && completed if 'false'
+
+	 if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+	 	filteredTodos  = _.where(filteredTodos, {completed: true});
+	 } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos  = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
+
+});
+
 // GET /todos/:id
-app.get('/todos/:id', function (req, res) {
+app.get('/todos/:id', function (req, res) {	
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
