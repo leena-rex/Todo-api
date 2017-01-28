c74631e931aa0d72b76c41b141bb04fceb888fce
Leena Rex <lrex@silverlink.com>
2017-01-28 15:33:46 -0500

    convert PUT /tod/:id

--- server.js
+++ server.js
@@ -36,25 +36,7 @@ app.get('/todos', function(req, res) {
 	}, function (e) {
 
 	})
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: false
-	// 	});
-	// }
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-	// 	});
-	// }
-
-	// res.json(filteredTodos);
+	
 });
 
 // GET /todos/:id
@@ -86,21 +68,7 @@ app.post('/todos', function(req, res) {
 
 	}), function (e) {
 		res.status(400).json(e);
-	}
-
-	
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-	// res.json(body);
-
+	}	
 
 });
 
@@ -126,47 +94,41 @@ app.delete('/todos/:id', function(req, res) {
 		res.status(500).send();
 	});
 
-	// var matchedTodo = _.findWhere(todos, {
-	// 	id: todoId
-	// });
-
-	// if (!matchedTodo) {
-	// 	res.status(404).json({
-	// 		"error": "no todo found with that id"
-	// 	});
-	// } else {
-	// 	todos = _.without(todos, matchedTodo);
-	// 	res.json(matchedTodo);
-	// }
+	
 });
 
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
-	}
+	if (body.hasOwnProperty('completed') ) {
+		attributes.completed = body.completed;
+	} 
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
+	if (body.hasOwnProperty('description') ) {
+		attributes.description = body.description;
+	} 
 
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+	db.todo.findById(todoId).then ( function(todo) {
+		if (todo) {
+			//chain the promises
+			todo.update(attributes).then(function (todo){
+			res.json(todo.toJSON());
 
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+			}, function (e) {
+				res.status(400).json(e);
+
+			});
+		} else {
+			res.status(404).send();
+		}
+		//ID update fails
+	}, function () {
+		res.status(500).send();
+	});
+
 });
 
 db.sequelize.sync().then(function() {
