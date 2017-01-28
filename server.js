dd6c77d93fcceea7dcebaf2544ade895c895326c
Leena Rex <lrex@silverlink.com>
2017-01-28 17:09:57 -0500

    hash and salt password

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.post('/users', function(req, res) {
 	// else cathe (e) and pass it to res.json(e)
 
 	db.user.create(body).then (function (user){
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 
 	}), function (e) {
 		res.status(400).json(e);
@@ -148,7 +148,7 @@ app.post('/users', function(req, res) {
 
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
