4ee0a13c61a171a35ef78d668903985df7654ad7
Leena Rex <lrex@silverlink.com>
2017-01-30 19:07:42 -0500

    modified generate token

--- server.js
+++ server.js
@@ -153,28 +153,20 @@ app.post('/users', function(req, res) {
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
-	if ( typeof body.email !== 'string' || typeof body.password !== 'string' ) {
-		return res.status(400).send();
 
-	} 
+	db.user.authenticate(body).then(function () {
+		var token = user.generateToken('authentication');
 
-	db.user.findOne({
-		where: {
-			email: body.email
-		}
-	}).then (function (user) {		
+		if (token) {
+		res.header('Auth', token).json(user.toPublicJSON());
+	} else {
+		res.status(401).send();
+	}
+	}, function () {
+		res.status(401).send();
+	});
 
-		if (!user || bcrypt.compareSync(body.password, user.get('password_hash'))) {
-			return res.status(401).send();
-		}
-
-		res.json(user.toPublicJSON());
-
-	}), function (e) {
-		res.status(500).send();
-	};
-
-	res.json(body);
+	
  });
 
 db.sequelize.sync({force: true}).then(function() {
